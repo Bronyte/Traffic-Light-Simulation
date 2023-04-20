b3fa2ad93bd9f4e14bd56a7d1e3a34a0fd4203c6
Bronyte <jgriebel.namibia@gmail.com>
2023-04-20 23:20:20 +0200

    Add project files.

---
+++ Traffic Light Simulation/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        GroupBox1 = New GroupBox()
+        Button_Yellow = New Button()
+        Button_Green = New Button()
+        Button_Red = New Button()
+        BindingSource1 = New BindingSource(components)
+        GroupBox2 = New GroupBox()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        TextBox_Green = New TextBox()
+        Button_Stop = New Button()
+        Button_Start = New Button()
+        Label5 = New Label()
+        TextBox_Yellow = New TextBox()
+        TextBox_Red = New TextBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label1 = New Label()
+        Panel1 = New Panel()
+        Timer1 = New Timer(components)
+        GroupBox1.SuspendLayout()
+        CType(BindingSource1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.BackColor = SystemColors.Control
+        GroupBox1.Controls.Add(Button_Yellow)
+        GroupBox1.Controls.Add(Button_Green)
+        GroupBox1.Controls.Add(Button_Red)
+        GroupBox1.Location = New Point(295, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(141, 158)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Set Colour"
+        ' 
+        ' Button_Yellow
+        ' 
+        Button_Yellow.BackColor = Color.Yellow
+        Button_Yellow.Location = New Point(34, 72)
+        Button_Yellow.Name = "Button_Yellow"
+        Button_Yellow.Size = New Size(75, 23)
+        Button_Yellow.TabIndex = 1
+        Button_Yellow.Text = "Yellow"
+        Button_Yellow.UseVisualStyleBackColor = False
+        ' 
+        ' Button_Green
+        ' 
+        Button_Green.BackColor = Color.FromArgb(CByte(128), CByte(255), CByte(128))
+        Button_Green.Location = New Point(34, 113)
+        Button_Green.Name = "Button_Green"
+        Button_Green.Size = New Size(75, 23)
+        Button_Green.TabIndex = 1
+        Button_Green.Text = "Green"
+        Button_Green.UseVisualStyleBackColor = False
+        ' 
+        ' Button_Red
+        ' 
+        Button_Red.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        Button_Red.Location = New Point(34, 33)
+        Button_Red.Name = "Button_Red"
+        Button_Red.Size = New Size(75, 23)
+        Button_Red.TabIndex = 1
+        Button_Red.Text = "Red"
+        Button_Red.UseVisualStyleBackColor = False
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.BackColor = SystemColors.Control
+        GroupBox2.Controls.Add(Label8)
+        GroupBox2.Controls.Add(Label7)
+        GroupBox2.Controls.Add(Label6)
+        GroupBox2.Controls.Add(TextBox_Green)
+        GroupBox2.Controls.Add(Button_Stop)
+        GroupBox2.Controls.Add(Button_Start)
+        GroupBox2.Controls.Add(Label5)
+        GroupBox2.Controls.Add(TextBox_Yellow)
+        GroupBox2.Controls.Add(TextBox_Red)
+        GroupBox2.Controls.Add(Label4)
+        GroupBox2.Controls.Add(Label3)
+        GroupBox2.Controls.Add(Label1)
+        GroupBox2.Location = New Point(295, 185)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(141, 166)
+        GroupBox2.TabIndex = 0
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Cycle"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(111, 114)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(24, 15)
+        Label8.TabIndex = 5
+        Label8.Text = "sec"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(111, 88)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(24, 15)
+        Label7.TabIndex = 5
+        Label7.Text = "sec"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(111, 61)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(24, 15)
+        Label6.TabIndex = 5
+        Label6.Text = "sec"
+        ' 
+        ' TextBox_Green
+        ' 
+        TextBox_Green.Location = New Point(77, 111)
+        TextBox_Green.MaxLength = 1
+        TextBox_Green.Name = "TextBox_Green"
+        TextBox_Green.PlaceholderText = "2"
+        TextBox_Green.Size = New Size(32, 23)
+        TextBox_Green.TabIndex = 4
+        TextBox_Green.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Button_Stop
+        ' 
+        Button_Stop.Location = New Point(3, 140)
+        Button_Stop.Name = "Button_Stop"
+        Button_Stop.Size = New Size(58, 23)
+        Button_Stop.TabIndex = 3
+        Button_Stop.Text = "Stop"
+        Button_Stop.UseVisualStyleBackColor = True
+        ' 
+        ' Button_Start
+        ' 
+        Button_Start.Location = New Point(77, 140)
+        Button_Start.Name = "Button_Start"
+        Button_Start.Size = New Size(58, 23)
+        Button_Start.TabIndex = 3
+        Button_Start.Text = "Start"
+        Button_Start.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(30, 114)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(41, 15)
+        Label5.TabIndex = 2
+        Label5.Text = "Green:"
+        ' 
+        ' TextBox_Yellow
+        ' 
+        TextBox_Yellow.Location = New Point(77, 85)
+        TextBox_Yellow.MaxLength = 1
+        TextBox_Yellow.Name = "TextBox_Yellow"
+        TextBox_Yellow.PlaceholderText = "2"
+        TextBox_Yellow.Size = New Size(32, 23)
+        TextBox_Yellow.TabIndex = 4
+        TextBox_Yellow.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' TextBox_Red
+        ' 
+        TextBox_Red.Location = New Point(77, 58)
+        TextBox_Red.MaxLength = 1
+        TextBox_Red.Name = "TextBox_Red"
+        TextBox_Red.PlaceholderText = "2"
+        TextBox_Red.Size = New Size(32, 23)
+        TextBox_Red.TabIndex = 4
+        TextBox_Red.TextAlign = HorizontalAlignment.Center
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(27, 88)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(44, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "Yellow:"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(41, 61)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(30, 15)
+        Label3.TabIndex = 1
+        Label3.Text = "Red:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(6, 31)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(71, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Set Timings:"
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Location = New Point(12, 12)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(277, 339)
+        Panel1.TabIndex = 1
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(452, 360)
+        Controls.Add(Panel1)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        CType(BindingSource1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button_Yellow As Button
+    Friend WithEvents Button_Green As Button
+    Friend WithEvents Button_Red As Button
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox_Green As TextBox
+    Friend WithEvents Button_Start As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox_Yellow As TextBox
+    Friend WithEvents TextBox_Red As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button_Stop As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Traffic Light Simulation/Form1.vb
@@ -0,0 +1,184 @@
+﻿'The main components Of the code are:
+'       +   The three Color variables (RedLight, YellowLight, And GreenLight) that store the current state Of the traffic lights.             
+'       +   The three Event handlers For the Red, Yellow, And Green buttons, which change the state Of the traffic lights And invalidate the Panel1 control To trigger a repaint.
+'                    -      These Colors set to ON/OFF
+'                                   Red = ON    , DarkRed = OFF
+'                                   Yellow = ON , Khaki = OFF
+'                                   Lime = On   , DarkGreen = OFF   
+'       +   The Panel1_Paint Event handler, which draws the traffic lights And robot frame Using the DrawRedLight, DrawYellowLight, DrawGreenLight, And DrawRobotFrame methods.
+'       +   The DrawRedLight, DrawYellowLight, DrawGreenLight, and DrawRobotFrame methods, which use the Graphics Class To draw the corresponding shapes And colors.
+
+Imports System.Drawing.Design
+
+Public Class Form1
+
+    'Traffic Lights
+    Dim RedLight As Color = Color.DarkRed
+    Dim YellowLight As Color = Color.Khaki
+    Dim GreenLight As Color = Color.DarkGreen
+    'Traffic Light Frame
+    Dim RobotFrame As Color = Color.Black
+    'Cycle Status - TRUE when "start" is clicked, FALSE when "stop" is clicked
+    Dim IsCycling As Boolean = False
+
+    'Timer to control traffic light timing
+    Dim TrafficLightTimer As New Timer()
+    Dim TickCounter As Integer
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.Invalidate()
+        'Initialize timer
+        TrafficLightTimer.Interval = 1000 '1 seconds
+        AddHandler TrafficLightTimer.Tick, AddressOf Timer1_Tick
+        TickCounter = 0
+    End Sub
+
+    Private Sub Button_Red_Click(sender As Object, e As EventArgs) Handles Button_Red.Click
+        'Set Traffic Light to RED
+        RedLight = Color.Red            'ON
+        YellowLight = Color.Khaki       'OFF
+        GreenLight = Color.DarkGreen    'OFF
+        Panel1.Invalidate()
+        'Stop timer if it's running
+        If TrafficLightTimer.Enabled Then
+            TrafficLightTimer.Stop()
+        End If
+    End Sub
+
+    Private Sub Button_Yellow_Click(sender As Object, e As EventArgs) Handles Button_Yellow.Click
+        'Set Traffic Light To YELLOW
+        RedLight = Color.DarkRed        'OFF
+        YellowLight = Color.Yellow      'ON
+        GreenLight = Color.DarkGreen    'OFF
+        Panel1.Invalidate()
+        'Stop timer if it's running
+        If TrafficLightTimer.Enabled Then
+            TrafficLightTimer.Stop()
+        End If
+    End Sub
+
+    Private Sub Button_Green_Click(sender As Object, e As EventArgs) Handles Button_Green.Click
+        'Set Traffic Light To GREEN
+        RedLight = Color.DarkRed        'OFF
+        YellowLight = Color.Khaki       'OFF
+        GreenLight = Color.Lime         'ON
+        Panel1.Invalidate()
+        'Stop timer if it's running
+        If TrafficLightTimer.Enabled Then
+            TrafficLightTimer.Stop()
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        'Draw Robot frame to prevent it from overlaying the traffic lights
+        DrawRobotFrame(e.Graphics, RobotFrame)
+        'Draw Traffic lights next
+        DrawRedLight(e.Graphics, RedLight)
+        DrawYellowLight(e.Graphics, YellowLight)
+        DrawGreenLight(e.Graphics, GreenLight)
+    End Sub
+
+
+
+
+    Private Sub DrawYellowLight(g As Graphics, color As Color)
+        Dim brush As New SolidBrush(YellowLight)
+        'Panel1 Dimensions:  (Width, Height) = (258, 336)
+
+        'Center of the panel are at: (258, 336)/2 = (129,168)
+
+        'The coordinates of the circle (top left vertex) is ( 129-width_of_circle/2 , 168 - height_of_circle/2)
+
+        'Dimensions of the Circle: ( Width , Height ) = ( 50, 50)
+        '129 - 50/2 = 104
+        '168 - 50/2 = 143
+        'Coordinates of the circle: ( X , Y ) = ( 104 , 143 )
+        g.FillEllipse(brush, 104, 143, 50, 50)
+    End Sub
+
+
+    Private Sub DrawRedLight(g As Graphics, color As Color)
+        Dim brush As New SolidBrush(RedLight)
+        'The Red light lies 50 + 10 points above the yellow light
+        'The coordinates of the top left vertex of the red light is at ( 104, 143 - 50 - 10 ) = ( 104 , 83 )
+        g.FillEllipse(brush, 104, 83, 50, 50)
+    End Sub
+
+    Private Sub DrawGreenLight(g As Graphics, color As Color)
+        Dim brush As New SolidBrush(GreenLight)
+        'The green light lies 50 + 10 points below the yellow light
+        'The coordinates of the top left vertex of the green light is at ( 104, 178 + 50 + 10 ) = ( 104 , 203 )
+        g.FillEllipse(brush, 104, 203, 50, 50)
+    End Sub
+
+    Private Sub DrawRobotFrame(g As Graphics, color As Color)
+        Dim brush As New SolidBrush(RobotFrame)
+        'Panel1 Dimensions:  (Width, Height) = (258, 336)
+
+        'Center of the panel are at: (258, 336)/2 = (129,168)
+
+        'The rectangle will have an offset of 20 points from the circle.
+
+        'Dimensions of the Rectangle: ( Width , Height ) = ( 50 + 2*20, 3*50 + 2*20) = ( 90 , 190 )
+        '90/2 = 45 -> 129 - 45 = 84
+        '190/2 = 95 -> 168 - 95 = 73
+        g.FillRectangle(brush, 84, 73, 90, 190)
+    End Sub
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Dim RedTime As Integer = Convert.ToInt16(TextBox_Red.Text)
+            Dim YellowTime As Integer = Convert.ToInt16(TextBox_Yellow.Text)
+            Dim GreenTime As Integer = Convert.ToInt16(TextBox_Green.Text)
+
+            Dim untilyellow As Integer = RedTime
+            Dim untilgreen As Integer = RedTime + YellowTime
+            Dim untilred As Integer = RedTime + YellowTime + GreenTime
+
+            If IsCycling Then
+                If TickCounter >= untilred Then
+                    TickCounter = 0
+                ElseIf TickCounter <= untilyellow Then
+                    'Set Traffic Light to RED
+                    RedLight = Color.Red            'ON
+                    YellowLight = Color.Khaki       'OFF
+                    GreenLight = Color.DarkGreen    'OFF
+                    Panel1.Invalidate()
+                ElseIf TickCounter > untilyellow And TickCounter <= untilgreen Then
+                    'Set Traffic Light To YELLOW
+                    RedLight = Color.DarkRed        'OFF
+                    YellowLight = Color.Yellow      'ON
+                    GreenLight = Color.DarkGreen    'OFF
+                    Panel1.Invalidate()
+                ElseIf TickCounter > untilgreen And TickCounter <= untilred Then
+                    'Set Traffic Light To GREEN
+                    RedLight = Color.DarkRed        'OFF
+                    YellowLight = Color.Khaki       'OFF
+                    GreenLight = Color.Lime         'ON
+                    Panel1.Invalidate()
+                End If
+
+                TickCounter += 1
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button_Start_Click(sender As Object, e As EventArgs) Handles Button_Start.Click
+        'Start cycling through traffic lights
+        IsCycling = True
+        'Start timer
+        TrafficLightTimer.Start()
+    End Sub
+
+    Private Sub Button_Stop_Click(sender As Object, e As EventArgs) Handles Button_Stop.Click
+        'Stop cycling through traffic lights
+        IsCycling = False
+        'Stop timer
+        TrafficLightTimer.Stop()
+    End Sub
+End Class
